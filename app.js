cc480287e47021510df3898b171a29ebf9d9cecc
Marcelo1976H <163341810+Marcelo1976H@users.noreply.github.com>
2025-09-17 23:16:21 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaAmigos = [];
+let numeroEscolhido = 0;
+let nomeAmigos = "";
+
+function TextoInicial(){
+    TextoResultado('h2','Digite o nome dos seus amigos')
+}
+
+function TextoResultado(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+function adicionarAmigo() {
+let nomeAmigos = document.querySelector('input').value;
+    listaAmigos.push(nomeAmigos.trim());
+    limparCampo();
+};
+
+function limparCampo() {
+    nomeAmigos = document.querySelector('input');
+    nomeAmigos.value = '';
+}
+
+function sortearAmigo() {
+if (listaAmigos.length === 0) {
+    alert ("Digite um nome válido!");
+    return;
+}
+   let numeroEscolhido = Math.floor(Math.random() * listaAmigos.length);
+   let nomeEscolhido = listaAmigos [numeroEscolhido];
+   TextoResultado('h2',`O nome sorteado é: ${nomeEscolhido}`);
+   listaAmigos = [];
+    //TextoInicial();//
+}
+
+function reiniciarSorteio() {
+    TextoInicial();
+//    listaAmigos = [];//
+    }
